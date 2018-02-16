a7523c2cc40c1e4baf776565370ef9fda461a5e7
Michael Andreasen <contact@mandreasen.com>
2018-02-16 21:08:17 +0100

    Code upload

---
+++ index.js
@@ -0,0 +1,135 @@
+const OS = require('os');
+const HTTPS = require('https');
+const Crypto = require('crypto');
+const QueryString = require('querystring');
+
+const REPORT_HOST = "localhost";
+const REPORT_PATH = "/report.php";
+
+class Reporter {
+	constructor(packageJson) {
+		// Show debug output. Default: false.
+		this.debug = process.env.MA_NODEJS_STATS_REPORTER_DEBUG || false;
+
+		// Report stats. Default: true.
+		this.report = process.env.MA_NODEJS_STATS_REPORTER_REPORT || true;
+
+		// Machine identifier.
+		this.machineID = this.getMachineID();
+
+		// Start up timestamp.
+		this.startupTimestamp = Math.floor(Date.now() / 1000);
+
+		// Reporter version
+		this.reporterVersion = require('./package.json').version || "unknown";
+
+		// Modules there is reporting stats.
+		this.modules = {};
+	}
+
+	setup(packageJson) {
+		// Start reporting if not started yet.
+		if (Object.keys(this.modules).length <= 0) {
+			// Setup stats report.
+			this.setupReporting();
+		}
+
+		// Add module to the modules list if not added yet.
+		if (!this.modules.hasOwnProperty(packageJson.name)) {
+			this.modules[packageJson.name] = {
+				name: packageJson.name,
+				version: packageJson.version
+			};
+		}
+	}
+
+	getMachineID() {
+		var macs = [];
+		var iface = null;
+		var interfaces = OS.networkInterfaces();
+
+		for (var ifName in interfaces) {
+			iface = interfaces[ifName];
+
+			iface.forEach(function(virtualInterface) {
+				if (virtualInterface.mac != "00:00:00:00:00:00" && macs.indexOf(virtualInterface.mac) == -1) {
+					macs.push(virtualInterface.mac);
+				}
+			});
+		}
+
+		macs.sort();
+		var hash = Crypto.createHash('sha1');
+		hash.update(macs.join(','), 'ascii');
+		var id = hash.digest('hex');
+
+		if (this.debug) {
+			console.log("Machine identifier:", id);
+		}
+
+		return id;
+	}
+
+	setupReporting() {
+		if (this.debug) {
+			// Report stats immediately in 5 seconds.
+			setTimeout(this.reportStats.bind(this), 1000 * 5);
+		} else if (this.report) {
+			// Report stats hourly.
+			setInterval(this.reportStats.bind(this), 1000 * 60 * 60);
+
+			// Report stats immediately 15 minutes after start.
+			setTimeout(this.reportStats.bind(this), 1000 * 60 * 15);
+		}
+	}
+
+	reportStats() {
+		if (this.report) {
+			var stats = {
+				node_version: process.versions.node,
+				reporter_version: this.reporterVersion,
+				machine_identifier: this.machineID,
+				os_platform: OS.platform(),
+				os_release: OS.release(),
+				app_uptime: Math.floor(Date.now() / 1000) - this.startupTimestamp
+			};
+
+			for (var module in this.modules) {
+				stats["module_name"] = this.modules[module].name;
+				stats["module_version"] = this.modules[module].version;
+
+				var statString = QueryString.stringify(stats);
+
+				var request = HTTPS.request({
+					"method": "POST",
+					"hostname": REPORT_HOST,
+					"path": REPORT_PATH,
+					"headers": {
+						"Content-Type": "application/x-www-form-urlencoded",
+						"Content-Length": Buffer.byteLength(statString),
+						"User-Agent": "node/" + process.versions.node + " nodejs-stats-reporter/" + this.reporterVersion
+					}
+				}, (result) => {
+					result.on('data', (ckunk) => {
+						if (this.debug && chunk.length > 0) {
+							console.log(chunk.toString('ascii'));
+						}
+					});
+
+					if (this.debug) {
+						console.log("-----------------------------------------------------------");
+						console.log("Stats reported for " + stats.module_name + "@" + stats.module_version);
+						console.log("Status code:", result.statusCode);
+						console.log(stats);
+						console.log("-----------------------------------------------------------");
+					}
+				});
+
+				request.on('error', () => {});
+				request.end(statString);
+			}
+		}
+	}
+}
+
+module.exports = new Reporter();
