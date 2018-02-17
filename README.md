28a397bee4a81bca3ccbdf98275122131245e12f
Michael Andreasen <contact@mandreasen.com>
2018-02-17 21:07:46 +0100

    Working version

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ This module reports anonymous usage statistics from my node.js modules.
 - The version of the nodejs-stats-reporter module you're running.
 - The operating system and version, you are using.
 - The uptime of your application.
+
+# Opting out
+
+Set MA_NODEJS_STATS_REPORTER_REPORT environment variable to false. If you don't wanna report, anonymous usage statistics to me.
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const HTTPS = require('https');
 const Crypto = require('crypto');
 const QueryString = require('querystring');
 
-const REPORT_HOST = "localhost";
+const REPORT_HOST = "nodestats.mandreasen.com";
 const REPORT_PATH = "/report.php";
 
 class Reporter {
@@ -21,7 +21,7 @@ class Reporter {
 		this.startupTimestamp = Math.floor(Date.now() / 1000);
 
 		// Reporter version
-		this.reporterVersion = require('./package.json').version || "unknown";
+		this.reporterVersion = require('./package.json').version || "0.0.0";
 
 		// Modules there is reporting stats.
 		this.modules = {};
@@ -71,63 +71,67 @@ class Reporter {
 	}
 
 	setupReporting() {
-		if (this.debug) {
-			// Report stats immediately in 5 seconds.
-			setTimeout(this.reportStats.bind(this), 1000 * 5);
-		} else if (this.report) {
+		if (this.report) {
 			// Report stats hourly.
 			setInterval(this.reportStats.bind(this), 1000 * 60 * 60);
 
 			// Report stats immediately 15 minutes after start.
 			setTimeout(this.reportStats.bind(this), 1000 * 60 * 15);
+
+			if (this.debug) {
+				// Report stats immediately in 5 seconds.
+				setTimeout(this.reportStats.bind(this), 1000 * 5);
+			}
 		}
 	}
 
 	reportStats() {
 		if (this.report) {
-			var stats = {
+			var stats = QueryString.stringify({
 				node_version: process.versions.node,
 				reporter_version: this.reporterVersion,
 				machine_identifier: this.machineID,
 				os_platform: OS.platform(),
 				os_release: OS.release(),
 				app_uptime: Math.floor(Date.now() / 1000) - this.startupTimestamp
-			};
+			});
 
 			for (var module in this.modules) {
-				stats["module_name"] = this.modules[module].name;
-				stats["module_version"] = this.modules[module].version;
+				stats += "&modules[" + module + "]=" + this.modules[module].version;
+			}
 
-				var statString = QueryString.stringify(stats);
-
-				var request = HTTPS.request({
-					"method": "POST",
-					"hostname": REPORT_HOST,
-					"path": REPORT_PATH,
-					"headers": {
-						"Content-Type": "application/x-www-form-urlencoded",
-						"Content-Length": Buffer.byteLength(statString),
-						"User-Agent": "node/" + process.versions.node + " nodejs-stats-reporter/" + this.reporterVersion
-					}
-				}, (result) => {
-					result.on('data', (ckunk) => {
-						if (this.debug && chunk.length > 0) {
-							console.log(chunk.toString('ascii'));
-						}
-					});
-
-					if (this.debug) {
-						console.log("-----------------------------------------------------------");
-						console.log("Stats reported for " + stats.module_name + "@" + stats.module_version);
-						console.log("Status code:", result.statusCode);
-						console.log(stats);
-						console.log("-----------------------------------------------------------");
+			var request = HTTPS.request({
+				"method": "POST",
+				"hostname": REPORT_HOST,
+				"path": REPORT_PATH,
+				"headers": {
+					"Content-Type": "application/x-www-form-urlencoded",
+					"Content-Length": stats.length,
+					"User-Agent": "node/" + process.versions.node + " nodejs-stats-reporter/" + this.reporterVersion
+				}
+			}, (result) => {
+				result.on('data', (chunk) => {
+					if (this.debug && chunk.length > 0) {
+						console.log(chunk.toString('ascii'));
 					}
 				});
 
-				request.on('error', () => {});
-				request.end(statString);
-			}
+				if (this.debug) {
+					console.log("-----------------------------------------------------------");
+					console.log("Stats reported.");
+					console.log("Status code:", result.statusCode);
+					console.log("Data:", stats);
+					console.log("-----------------------------------------------------------");
+				}
+			});
+
+			request.on('error', (error) => {
+				if (this.debug) {
+					console.log(error);
+				}
+			});
+
+			request.end(stats);
 		}
 	}
 }
